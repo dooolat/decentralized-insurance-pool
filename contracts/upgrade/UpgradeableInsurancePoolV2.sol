b9d9c36a2ee468c4df78166705f217c5535e27d1
Dulat Zhambyl <240469@astanait.edu.kz>
2026-05-15 21:15:56 +0500

    feat(upgrade): add UUPS insurance pool versions

--- contracts/upgrade/UpgradeableInsurancePoolV2.sol
+++ contracts/upgrade/UpgradeableInsurancePoolV2.sol
@@ -1,6 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract UpgradeableInsurancePoolV2 {
-    // TODO: implement UUPS V2 skeleton.
+import { UpgradeableInsurancePoolV1 } from "./UpgradeableInsurancePoolV1.sol";
+
+contract UpgradeableInsurancePoolV2 is UpgradeableInsurancePoolV1 {
+    uint256 public maxPolicyDuration;
+
+    event MaxPolicyDurationUpdated(uint256 maxPolicyDuration);
+
+    function initializeV2(
+        uint256 maxPolicyDuration_
+    ) external reinitializer(2) onlyOwner {
+        maxPolicyDuration = maxPolicyDuration_;
+        emit MaxPolicyDurationUpdated(maxPolicyDuration_);
+    }
+
+    function setMaxPolicyDuration(
+        uint256 newMaxPolicyDuration
+    ) external onlyOwner {
+        maxPolicyDuration = newMaxPolicyDuration;
+        emit MaxPolicyDurationUpdated(newMaxPolicyDuration);
+    }
+
+    function version() external pure override returns (string memory) {
+        return "v2";
+    }
 }
