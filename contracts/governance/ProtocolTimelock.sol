329307708149afff9088a239b866c1baf45726e4
Dulat Zhambyl <240469@astanait.edu.kz>
2026-05-15 21:14:01 +0500

    feat(governance): add protocol timelock

--- contracts/governance/ProtocolTimelock.sol
+++ contracts/governance/ProtocolTimelock.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract ProtocolTimelock {
-    // TODO: implement protocol timelock.
+import { TimelockController } from "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract ProtocolTimelock is TimelockController {
+    uint256 public constant MIN_DELAY_SECONDS = 2 days;
+
+    constructor(
+        address[] memory proposers,
+        address[] memory executors,
+        address admin
+    ) TimelockController(MIN_DELAY_SECONDS, proposers, executors, admin) { }
 }
